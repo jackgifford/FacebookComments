146cd0d6f27399d01e829c7e65da85586273b75e
Jack Gifford <jack.gifford2264@hotmail.com>
2019-05-18 16:26:33 +1000

    Added a gui

---
+++ Tree.Gui/App.xaml.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Tree.Gui
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        // Just like we do in mvc :)
+
+        public IConfiguration Configuration { get; private set; }
+        public IServiceProvider ServiceProvider { get; private set; }
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            var builder = new ConfigurationBuilder();
+
+            Configuration = builder.Build();
+
+            var serviceCollection = new ServiceCollection();
+
+            ConfigureServices(serviceCollection);
+
+            ServiceProvider = serviceCollection.BuildServiceProvider();
+            var main = ServiceProvider.GetRequiredService<MainWindow>();
+
+            main.Show();
+        }
+
+
+        private void ConfigureServices(IServiceCollection services)
+        {
+            services.AddTransient<MainWindow>();
+        }
+    }
+}
---
+++ Tree.Gui/MainWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Tree.Gui
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private string filePath;
+
+        private void Load_Click(object sender, RoutedEventArgs e)
+        {
+            var dialog = new Microsoft.Win32.OpenFileDialog();
+            dialog.DefaultExt = ".csv";
+            dialog.Filter = "CSV Files (*.csv)|*.csv";
+
+            var res = dialog.ShowDialog();
+
+            if (res == true)
+            {
+                FilePath.Text = $"File Loaded: {dialog.FileName}";
+                filePath = dialog.FileName;
+                Calc.IsEnabled = true;
+            }
+
+        }
+
+        private void Calc_Click(object sender, RoutedEventArgs e)
+        {
+            
+        }
+    }
+}
